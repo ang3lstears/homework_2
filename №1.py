39e96d61cf1ad4ba3beb921e4365b0a1f628afcc
ang3lstears <tsaivai@mail.ru>
2024-09-27 00:03:50 +0300

    Add files via upload

---
+++ №1.py
@@ -0,0 +1,6 @@
+a = float(input("введите первое число \n"))
+b = float(input("введите второе число \n"))
+if a != 0 and b != 0 :
+    print(a/b)
+    print(b/a)
+else: print("на ноль делить нельзя")
